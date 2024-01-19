cd50094c17fdc5dcbdb3cea194d48c63b7bce994
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-01-18 23:50:03 -0600

    Studio Exercise String and Array

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,14 +1,35 @@
+const input = require('readline-sync');
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+food = food.split(",").sort();
+equipment = equipment.split(",").sort();
+pets = pets.split(",").sort();
+sleepAids = sleepAids.split(",").sort();
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+// let cargoHold = [food,equipment,pets,sleepAids]
+let cargoHold = []
+cargoHold.push(food,equipment,pets,sleepAids)
+// console.log(cargoHold)
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+let cabinetNumber = input.question("Select a cabinet (0 - 3) in the cargoHold : ");
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
+if(cabinetNumber > cargoHold.length - 1){
+    console.log("Please enter cabinet number between (0-3)");
+} else {
+    console.log(`Cabinet ${cabinetNumber} has ${cargoHold[cabinetNumber]}`);
+}
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+let itemNumber = input.question(`Select the item number (0 - ${cargoHold[cabinetNumber].length - 1}) : `);
+if(itemNumber > cargoHold[cabinetNumber] - 1){
+    console.log(`Please enter Item number between (0 - ${cargoHold[cabinetNumber] - 1})`);
+} else {
+    console.log(`Cabinet ${cabinetNumber} has item ${itemNumber} as ${cargoHold[cabinetNumber][itemNumber]}`);
+}
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -9,3 +9,16 @@ let str = "LaunchCode";
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+let str1 = str.slice(3)+str.slice(0,3);
+console.log(str1);
+console.log(`Here is the value of current string : ${str}, And the value of modifies string : ${str1}.`);
+
+let numOfLetters = input.question("Enter number of letter to be relocated");
+if (numOfLetters > str.length ){
+    console.log(`Erorr in the input: Number of letter ${numOfLetters} is greater than string length ${str.length}`);
+    numOfLetters = 3
+}
+
+let newStr = str.slice(numOfLetters) + str.slice(0,numOfLetters);
+console.log(`Current Value of str is ${str} and the new value is ${newStr}`);
