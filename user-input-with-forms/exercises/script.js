00bd8518fadea6a4e495d5a7bb0a9cd8d71796fe
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-02-29 23:51:33 -0600

    script.js is updated!

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,21 @@
 //Code Your Solution Below
+window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        let testNameInput = document.querySelector(input["name=testName"]);
+        let boosterCountInput = document.querySelector(input["name=boosterCount"]);
+        let testDateInput = document.querySelector(input["name=testDate"]);
+        let rocketTypeInput = document.querySelector(input["name=rocketType"]);
+        let productionServersInput = document.querySelector(input["name=productionServersInput"]);
+        if (testNameInput.value === "" ||
+            boosterCountInput.value === 0 ||
+            testDateInput.value === 0 ||
+            rocketTypeInput.value==="" ||
+            productionServersInput.value === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+        };
+        
+        
+    });
+})
